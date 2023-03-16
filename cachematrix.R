86141e5220dbe60681016cd8c50f6782bda338f3
jziede <126026816+jziede@users.noreply.github.com>
2023-03-16 15:38:36 -0400

    Think this is it!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##I wrote this function to be as close to makeVector as possible while just 
+##swapping out "mean" for "inverse" and "m" for "inv"
+library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function()x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() {
+    inver <- ginv(x)  ## This part is different becaue when I used to getinv part,
+    inver%*%x ## it gave me null as a response, so I put in ginv from the MASS
+  }  ## package to get the inverse, and then it worked
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+##I wrote this function to be as close to cachemean as possible while just 
+##swapping out "mean" for "inverse" and "m" for "inv"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- inverse(data, ...)
+  x$setinv(inv)
+  inv
 }
